d689985f11f910c2fc59ae719a514fa2b83c63ac
fishenzon <Fishenzon@users.noreply.github.com>
2021-03-10 14:38:13 +0200

    Add files via upload

--- zips/script.module.myresolver/resolvers/euronews.py
+++ zips/script.module.myresolver/resolvers/euronews.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-import json, common
+import common
 
 def Resolve(channel):
 	url = 'https://{0}.euronews.com/api/watchlive.json'.format(channel)
 	UA = common.GetUserAgent()
 	headers = {'User-Agent': UA}
-	prms = json.loads(common.OpenURL(url, headers=headers))
-	prms = json.loads(common.OpenURL('https:{0}'.format(prms['url']), headers=headers))
-	return '{0}|User-Agent={1}'.format(prms['primary'], UA)
+	prms = common.OpenURL(url, headers=headers, responseMethod='json')
+	if prms['url'] != None:
+		prms = common.OpenURL('https:{0}'.format(prms['url']), headers=headers, responseMethod='json')
+		url = '{0}|User-Agent={1}'.format(prms['primary'], UA)
+	else:
+		url = 'plugin://plugin.video.youtube/play/?video_id={0}'.format(prms['videoId'])
+	return url
--- zips/script.module.myresolver/resolvers/mediabay.py
+++ zips/script.module.myresolver/resolvers/mediabay.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import json, common
+import common
 
 def Resolve(channel):
 	url = 'https://api.mediabay.tv/v2/channels/thread/{0}'.format(channel)
 	UA = common.GetUserAgent()
 	headers = {'User-Agent': UA}
-	prms = json.loads(common.OpenURL(url, headers=headers))
+	prms = common.OpenURL(url, headers=headers, responseMethod='json')
 	return '{0}|User-Agent={1}'.format(prms['data'][0]['threadAddress'], UA)
